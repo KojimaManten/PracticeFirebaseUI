830fa04d209af1ec438cb7cbccba3ec983d23f58
KojimaManten <jumper.manten@icloud.com>
2020-07-25 21:27:09 +0900

    Initial Commit

---
+++ PracticeFirebaseUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeFirebaseUI
+//
+//  Created by 小島満天 on 2020/07/25.
+//  Copyright © 2020 Manchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
